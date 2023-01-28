f5b61a74cf39054ec2e3b90b359ed6357ac1676a
rafalmoneta <rafalmoneta5@gmail.com>
2023-01-28 21:03:00 +0100

    apollo

--- fitmeet_hack/src/components/dashboard/FeedNewest.tsx
+++ fitmeet_hack/src/components/dashboard/FeedNewest.tsx
@@ -1,4 +1,11 @@
+import { useQuery } from "@apollo/client";
+import { GET_ALL_USERS } from "../../graphql/users";
+
 const FeedNewest = () => {
+  const query = useQuery(GET_ALL_USERS);
+
+  console.log(query);
+
   return (
     <div>
       <div className="">No newest people for now</div>
--- fitmeet_hack/src/index.tsx
+++ fitmeet_hack/src/index.tsx
@@ -10,7 +10,6 @@ import FeedMenu from "./components/dashboard/FeedMenu";
 import FeedUserProfile from "./components/dashboard/FeedUserProfile";
 import FeedCategories from "./components/dashboard/FeedCategories";
 import FeedMap from "./components/dashboard/FeedMap";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const IndexPage: React.FC = () => {
   const [wasUserLogIn, setWasUserLogIn] = useState<boolean>(true);
