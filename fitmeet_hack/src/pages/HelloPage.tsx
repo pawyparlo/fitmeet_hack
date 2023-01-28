98e18556d2ac57536968c7a89af606769f14b1c7
Pawyparlo <pawel.wyparlo@montroseosftware.com>
2023-01-28 13:33:15 +0100

    current work

--- fitmeet_hack/src/pages/HelloPage.tsx
+++ fitmeet_hack/src/pages/HelloPage.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import DashboardGird from "../components/DashboardGird";
+import { Layout, Row, Col, Button } from "antd";
+import "../styles/styles.scss";
+import "../styles/utils.scss";
 
 const HelloPage = () => {
   return (
-    <div>
-      <DashboardGird />
-    </div>
+    <Layout className="hello-page-layout">
+      <Row>
+        <Col span={24} className="center-horizontaly padding-bottom-20">
+          Get started
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24} className="center-horizontaly">
+          <Button type="primary">Get started</Button>
+        </Col>
+      </Row>
+    </Layout>
   );
 };
 
