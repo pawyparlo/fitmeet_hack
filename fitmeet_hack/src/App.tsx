40975e179482eb433cbbe3b9cb6722ab1904b759
rafalmoneta <rafalmoneta5@gmail.com>
2023-01-28 14:23:44 +0100

    feat: adding tailwindcss

--- fitmeet_hack/src/App.tsx
+++ fitmeet_hack/src/App.tsx
@@ -1,5 +1,6 @@
 import HelloPage from "./pages/HelloPage";
-import "./styles/styles.scss";
+// import "./styles/styles.scss";
+import "./styles/index.css";
 
 function App() {
   return <HelloPage />;
